d7f38c8dcfb14f2a415efb240019ad2cb2807bf3
madhubrjuno1 <madhumohanb@wavelabs.ai>
2023-03-01 17:08:40 +0530

    initial commit

---
+++ src/Streams/ConsumerInterface.java
@@ -0,0 +1,14 @@
+package Streams;
+
+import java.util.function.Consumer;
+
+public class ConsumerInterface {
+
+	
+	public static void main(String[] args)
+	{
+		Consumer<String> a =aa->System.out.println(aa);
+		
+		a.accept("[]iog");
+	}
+}
